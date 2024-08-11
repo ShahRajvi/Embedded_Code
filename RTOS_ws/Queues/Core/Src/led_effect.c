a839a1f5b39cd85975ff5e7d0c3c60cfe519a23a
Rajvi Shah <rajvishah2195@gmail.com>
2024-08-11 09:35:35 -0500

    Queues
    New project Queues created.
    Communicates over uart. Gets user data and process respective tasks. Queues implemented to read and write data to bus.
    task_handler.c contains all task definitions
    main.c and main.h updated.

---
+++ RTOS_ws/Queues/Core/Src/led_effect.c
@@ -0,0 +1,8 @@
+/*
+ * led_effect.c
+ *
+ *  Created on: Aug 11, 2024
+ *      Author: rajvi
+ */
+
+
---
+++ RTOS_ws/Queues/Debug/common/ThirdParty/FreeRTOS/timers.d
@@ -0,0 +1,26 @@
+common/ThirdParty/FreeRTOS/timers.o: \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/timers.c \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/queue.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/timers.h
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/queue.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/rajvi/OneDrive/Documents/00_Projects/Embedded_Code/RTOS_ws/common/ThirdParty/FreeRTOS/include/timers.h:
